d67b17510fac1dbbb5448616c0a7ebf307c1eee3
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-15 15:21:40 -0400

    Brew formula update for xavtool version 0.9.4-chocolatey

--- Formula/xavtool.rb
+++ Formula/xavtool.rb
@@ -1,9 +1,9 @@
 class Xavtool < Formula
   desc ""
   homepage ""
-  url "https://github.com/gabrielrobert/xavtool/releases/download/0.9.4/xavtool_0.9.4_Darwin_x86_64.tar.gz"
-  version "0.9.4"
-  sha256 "52f968308c00aba3ae1a32639b9b81c8e506a8f647762d7c9af3173b5962f808"
+  url "https://github.com/gabrielrobert/xavtool/releases/download/0.9.4-chocolatey/xavtool_0.9.4-chocolatey_Darwin_x86_64.tar.gz"
+  version "0.9.4-chocolatey"
+  sha256 "2aabd663c785c32ea30e0618b9132043f054b4418b3ac81d165df97ad7ab0e55"
 
   def install
     bin.install "xavtool"
