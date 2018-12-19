25ae79a4f16d4b5647fbaf39a6ca1a06d0c4485f
goreleaserbot <goreleaser@carlosbecker.com>
2018-12-19 16:40:56 -0500

    Brew formula update for xavtool version 1.2.1

--- Formula/xavtool.rb
+++ Formula/xavtool.rb
@@ -1,9 +1,9 @@
 class Xavtool < Formula
   desc ""
   homepage ""
-  url "https://github.com/gabrielrobert/xavtool/releases/download/1.2.0/xavtool_1.2.0_macOS_64-bit.tar.gz"
-  version "1.2.0"
-  sha256 "d69c471d52026123e2444688f97d4e91e0ba4207d99e7482f4bd16bc4d1099cb"
+  url "https://github.com/gabrielrobert/xavtool/releases/download/1.2.1/xavtool_1.2.1_macOS_64-bit.tar.gz"
+  version "1.2.1"
+  sha256 "28567b57a670ad05530b1c3e4d56c08877bb437ad3f8ff403928ad7c5d2be220"
 
   def install
     bin.install "xavtool"
